8152cddff8779d8c873b463acd8d977e118b4513
Brad <bradleykigsley2@gmail.com>
2021-02-07 11:52:06 +0300

    fix: adds check to admin routes, returning 404 if user is not logged in or not an admin

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,10 +6,22 @@ import AdminDashboard from '../pages/admin/dashboard';
 
 import {regularRoutes, adminRoutes} from './routes-list'
 import NotFound from '../pages/not-found';
+import {UserState} from '../state/reducers/userReducers';
+import {RootStateOrAny, useSelector} from 'react-redux';
 
 
 function Routes() {
+
   const AdminRoute = function ({ component: Component, ...rest }) {
+
+    const user: UserState = useSelector((state: RootStateOrAny) => state.user);
+
+    if (!user?.isLoggedIn || user?.role !== "ROLE_ADMIN"){
+      return (
+        <NotFound/>
+      )
+    }
+
     return (
       <Route
         {...rest}
