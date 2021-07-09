50a200194f7e6e8470fbe00478ab2c31ca5392c7
Keith <kazi.keith@gmail.com>
2021-07-09 16:17:06 +0300

    Admin can view product types (#104)
    * Admin can view product types
    
    - Admin can view single product type
    - Refactor product type form to be reusable
    
    * chore: remove old options
    
    * chore: minor changes
    
    Co-authored-by: Brad <bradleykigsley2@gmail.com>

--- src/pages/admin/productTypes/SingleProductType.tsx
+++ src/pages/admin/productTypes/SingleProductType.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
+import {useParams} from 'react-router';
+import useSWR from 'swr';
+import {EmptyState, Loading} from '../../../components';
+import {DeadEyes2} from '../../../constants/icons';
+import {useAuth} from '../../../hooks';
+import ProductTypeForm from './ProductTypeForm';
 
 export default function SingleProductType() {
-  return <h2>View Single Product Type</h2>;
+  const param: {slug: string} = useParams();
+  const api = useAuth();
+
+  const slug = param.slug || '';
+
+  const singleProductTypeFetcher = () =>
+    api.productTypes.getSingle(slug).then(({data}) => {
+      return data;
+    });
+
+  const {data, error} = useSWR(`productType/${slug}`, singleProductTypeFetcher);
+
+  if (error) {
+    return (
+      <EmptyState
+        icon={DeadEyes2}
+        centerAlign={true}
+        message={'An error occurred while fetching this product type: ' + error}
+        title={'Could not fetch product type'}
+      />
+    );
+  }
+
+  if (!data) {
+    return <Loading message={'Please wait a while...'} />;
+  }
+  return (
+    <ProductTypeForm
+      formAction={{action: 'view', formData: data}}
+      formTitle="Update a Product Type"
+    />
+  );
 }
