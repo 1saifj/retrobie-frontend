ef0710255df7fbdc398a3ee13c5bd537174f847d
Brad <bradleykigsley2@gmail.com>
2021-09-06 10:33:30 +0300

    chore(Layout): adds 'footer' setting

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -9,111 +9,111 @@ import {RootStateOrAny, useSelector} from 'react-redux';
 import {UserState} from '../../state/reducers/userReducers';
 import {Link} from 'react-router-dom';
 
-const Layout = function(
-  {
-      withoutNav,
-      topRightButton,
-      children,
-      style,
-      internal,
-  }: {
-      withoutNav?: boolean,
-      topRightButton?: boolean | Function,
-      children,
-      style?: Partial<CSSProperties>,
-      internal?: boolean,
-  }, ...props) {
+const Layout = function(params: {
+  withoutNav?: boolean,
+  topRightButton?: boolean | Function,
+  children,
+  style?: Partial<CSSProperties>,
+  internal?: boolean,
+  footer?: boolean;
+}, ...props) {
 
-    const warning = sessionStorage.getItem('hide-unverified-email-warning');
 
-    const [hideWarning, setWarningHidden] = useState(Boolean(warning));
+  const warning = sessionStorage.getItem('hide-unverified-email-warning');
 
-    const currentTheme = useSelector((state: RootStateOrAny) => state.meta.theme);
-    const user: UserState = useSelector((state: RootStateOrAny) => state.user);
+  const [hideWarning, setWarningHidden] = useState(Boolean(warning));
 
-    const [mounted, setMounted] = useState(false);
+  const currentTheme = useSelector((state: RootStateOrAny) => state.meta.theme);
+  const user: UserState = useSelector((state: RootStateOrAny) => state.user);
 
-    useEffect(() => {
-      setMounted(true);
-    }, []);
+  const [mounted, setMounted] = useState(false);
 
-    const defaultStyle = {
-        transition: `opacity 250ms ease-in-out`,
-        opacity: 0,
-        background: currentTheme === 'dark' ?
-          'var(--color-background--dark)' :
-          'var(--color-background--light)',
-    };
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-    const transitionStyles = {
-        entering: {opacity: 0},
-        entered: {opacity: 1},
-        exiting: {opacity: 0},
-        exited: {opacity: 0},
-    };
+  const defaultStyle = {
+    transition: `opacity 250ms ease-in-out`,
+    opacity: 0,
+    background: currentTheme === 'dark' ?
+      'var(--color-background--dark)' :
+      'var(--color-background--light)',
+  };
 
-    function hideUnverifiedEmailWarning() {
-        setWarningHidden(true);
-        sessionStorage.setItem('hide-unverified-email-warning', String(true));
-    }
+  const transitionStyles = {
+    entering: {opacity: 0},
+    entered: {opacity: 1},
+    exiting: {opacity: 0},
+    exited: {opacity: 0},
+  };
 
-    return (
-      <>
-          <Transition
-            in={mounted}
-            timeout={100}>
-              {
-                  state => (
-                    <div
-                      style={{...defaultStyle, ...transitionStyles[state]}}>
-                        <div
-                          className='layout--root'>
-                            <div>
-                                {
-                                    user.isLoggedIn && !user.isVerified && (
-                                      <Notification
-                                        style={{display: Boolean(hideWarning) ? 'none' : 'block'}}
-                                        isColor={'warning'}>
-                                          <Delete onClick={() => hideUnverifiedEmailWarning()} />
-                                          <div style={{display: 'flex'}}>
-                                              <img src={ErrorIconDark}
-                                                   style={{width: '24px', margin: '0 12px', display: 'inline'}}
-                                                   alt="error icon" />
-                                              <p>
-                                                  Psst. You haven't verified your email yet.
-                                                  Some functionality will be limited until you do.
-                                              </p>
-                                              <p>
-                                                  To confirm your email address, visit{' '}
-                                                  <Link to={'/accounts/verify?sendCode=true'}>
-                                                      this link
-                                                  </Link>
-                                              </p>
-                                          </div>
-                                      </Notification>
-                                    )
-                                }
+  function hideUnverifiedEmailWarning() {
+    setWarningHidden(true);
+    sessionStorage.setItem('hide-unverified-email-warning', String(true));
+  }
 
-                                <Header
-                                  withoutNav={withoutNav}
-                                  topRightButton={topRightButton} />
-                                <LayoutParent
-                                  className='layout--parent' {...props}
-                                  style={{...style}}>
-                                    {children}
-                                </LayoutParent>
-                                <div style={{marginTop: 48}}>
-                                    <Footer internal={internal} />
-                                </div>
-                            </div>
+  return (
+    <>
+      <Transition
+        in={mounted}
+        timeout={100}>
+        {
+          state => (
+            <div
+              style={{...defaultStyle, ...transitionStyles[state]}}>
+              <div
+                className='layout--root'>
+                <div>
+                  {
+                    user.isLoggedIn && !user.isVerified && (
+                      <Notification
+                        style={{display: Boolean(hideWarning) ? 'none' : 'block'}}
+                        isColor={'warning'}>
+                        <Delete onClick={() => hideUnverifiedEmailWarning()} />
+                        <div style={{display: 'flex'}}>
+                          <img src={ErrorIconDark}
+                               style={{width: '24px', margin: '0 12px', display: 'inline'}}
+                               alt="error icon" />
+                          <p>
+                            Psst. You haven't verified your email yet.
+                            Some functionality will be limited until you do.
+                          </p>
+                          <p>
+                            To confirm your email address, visit{' '}
+                            <Link to={'/accounts/verify?sendCode=true'}>
+                              this link
+                            </Link>
+                          </p>
                         </div>
-                    </div>
-                  )
-              }
-          </Transition>
+                      </Notification>
+                    )
+                  }
 
-      </>
-    );
+                  <Header
+                    withoutNav={params.withoutNav}
+                    topRightButton={params.topRightButton} />
+                  <LayoutParent
+                    className='layout--parent' {...props}
+                    style={{...(params.style)}}>
+                    {params.children}
+                  </LayoutParent>
+                  {
+                    !!params.footer && (
+                      <div style={{marginTop: 48}}>
+                        <Footer internal={params.internal} />
+                      </div>
+                    )
+                  }
+
+                </div>
+              </div>
+            </div>
+          )
+        }
+      </Transition>
+
+    </>
+  );
 
 };
 
