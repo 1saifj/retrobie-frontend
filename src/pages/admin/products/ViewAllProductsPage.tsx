f90db6cea8584e86c543a2be1d9b13dd0ad8411b
Brad <bradleykigsley2@gmail.com>
2021-09-09 12:57:53 +0300

    feat(ViewAllProductsPage): adds empty state section for empty products

--- src/pages/admin/products/ViewAllProductsPage.tsx
+++ src/pages/admin/products/ViewAllProductsPage.tsx
@@ -28,21 +28,11 @@ export default function ViewAllProductsPage() {
 
   const [filteredProducts, setFilteredProducts] = useState([]);
 
-  const [showSelectedImageModal, setShowSelectedImageModal] = useState(false);
-  const [selectedImages, setSelectedImages] = useState([]);
-  const [isCreateProductModalActive, setCreateProductModalActive] = useState(false);
-
-
-  const dispatch = useDispatch();
-  const notify = useNotify();
-
-  const [indexingProducts, setIndexingProducts] = useState(false);
-
   useEffect(()=> {
     setFilteredProducts(allProducts)
   }, [allProducts])
 
-  if (!allProducts?.length) {
+  if (!allProducts) {
     return (
       <div>
         <Loading minor={true} />
@@ -72,7 +62,7 @@ export default function ViewAllProductsPage() {
         <Column isSize={{default: 7}} className="mb-4">
           <label>Filter products by name</label>
           <Input
-            placeholder={"eg. Kobe, Lebron, etc."}
+            placeholder={'eg. Kobe, Lebron, etc.'}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
               const input = e.target.value;
               filterProductsByValue(input);
@@ -81,81 +71,112 @@ export default function ViewAllProductsPage() {
           </Input>
         </Column>
         <Column>
-          <div className="buttons mt-4 is-flex">
-            <Button
-              isLoading={indexingProducts}
-              isColor="light"
-              onClick={async () => {
-                setIndexingProducts(true)
-                try {
-                  await dispatch(api.products.reIndex({
-                    deleteAll: true,
-                    createIndexIfNotExists: true
-                  }))
-                  notify.success("Indexed products successfully")
-                }catch (e){
-                  const message = extractErrorMessage(e);
-                  notify.error(message);
-                }
-                setIndexingProducts(false)
-              }}
-            >
-              Re-index products
-            </Button>
-            <Button
-              onClick={()=> setCreateProductModalActive(true)}
-              isColor="light">
-              Add new Product
-            </Button>
-
-          </div>
-
+          <ProductPageHeader />
         </Column>
       </Columns>
 
       <div>
-        <div>
-
-          <CreateProductModal
-            isActive={isCreateProductModalActive}
-            onClickSelectedImage={images => {
-              setShowSelectedImageModal(true);
-              setSelectedImages(images);
-            }}
-            onClose={() => {
-              setCreateProductModalActive(false)
-              setShowSelectedImageModal(false);
-            }}
-          />
-          <SelectedImageModal
-            showModal={showSelectedImageModal}
-            images={selectedImages}
-            onClose={() => {
-              setShowSelectedImageModal(false);
-            }}
-          />
-        </div>
 
       </div>
 
       <div>
-        <div>
-          {
-            filteredProducts?.map(product => {
-              return (
+        {
+          filteredProducts?.length ? (
+              <div>
                 <div>
-                  <Link to={`products/${product.slug}`}>
-                    <SimpleListItem>
-                      <p>{product.name}</p>
-                    </SimpleListItem>
-                  </Link>
+                  <div>
+                    {
+                      filteredProducts.map(product => {
+                        return (
+                          <div>
+                            <Link to={`products/${product.slug}`}>
+                              <SimpleListItem>
+                                <p>{product.name}</p>
+                              </SimpleListItem>
+                            </Link>
 
+                          </div>
+                        );
+                      })
+                    }
+                  </div>
                 </div>
-              );
-            })
-          }
-        </div>
+              </div>
+            ) :
+            <div>
+              <p>No products uploaded yet.</p>
+            </div>
+        }
       </div>
+    </>
+  );
+};
+
+const ProductPageHeader = () => {
+
+  const dispatch = useDispatch();
+  const notify = useNotify();
+
+  const api = useApi();
+
+  const [indexingProducts, setIndexingProducts] = useState(false);
+
+  const [showSelectedImageModal, setShowSelectedImageModal] = useState(false);
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [isCreateProductModalActive, setCreateProductModalActive] = useState(false);
+
+  return (
+    <>
+      <div>
+
+        <CreateProductModal
+          isActive={isCreateProductModalActive}
+          onClickSelectedImage={images => {
+            setShowSelectedImageModal(true);
+            setSelectedImages(images);
+          }}
+          onClose={() => {
+            setCreateProductModalActive(false);
+            setShowSelectedImageModal(false);
+          }}
+        />
+        <SelectedImageModal
+          showModal={showSelectedImageModal}
+          images={selectedImages}
+          onClose={() => {
+            setShowSelectedImageModal(false);
+          }}
+        />
+      </div>
+      <div className="buttons mt-4 is-flex">
+        <Button
+          isLoading={indexingProducts}
+          isColor="light"
+          onClick={async () => {
+            setIndexingProducts(true);
+            try {
+              await dispatch(api.products.reIndex({
+                deleteAll: true,
+                createIndexIfNotExists: true,
+              }));
+              notify.success('Indexed products successfully');
+            } catch (e) {
+              const message = extractErrorMessage(e);
+              notify.error(message);
+            }
+            setIndexingProducts(false);
+          }}
+        >
+          Re-index products
+        </Button>
+        <Button
+          onClick={() => setCreateProductModalActive(true)}
+          isColor="light">
+          Add new Product
+        </Button>
+
+      </div>
+
     </>
   );
 };
