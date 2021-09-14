3fcdd40ae3fea292545e41b2e76c2311d575d9bf
Brad <bradleykigsley2@gmail.com>
2021-09-14 14:00:00 +0300

    feat(CreateVariantModal): adds functionality for creating variant

--- src/pages/admin/products/components/CreateVariantModal.tsx
+++ src/pages/admin/products/components/CreateVariantModal.tsx
@@ -1,59 +1,89 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Button, Column, Columns} from 'bloomer';
 import {FieldArray, Form, Formik} from 'formik';
 import '../../../../assets/style/bulma-fx';
 import 'react-quill/dist/quill.snow.css';
 import CustomModal from '../../../../components/CustomModal';
 import CreateVariantComponent from './CreateVariantComponent';
-import ImageUploader from '../../../../components/uploader/ImageUploader';
-import {useApi} from '../../../../hooks';
+import {useApi, useNotify} from '../../../../hooks';
 import useSWR from 'swr/esm/use-swr';
 import {ProductTypeType} from '../../../../types';
 
 
 const CreateVariantModal = (props: {
-  onClose: ()=> void,
+  onClose: () => void,
   isActive: boolean,
   // required when creating a new variant
   productTypeId?: string;
+  productId: string;
   // required when viewing/editing an existing variant
 }) => {
 
 
   const api = useApi();
 
+  const notify = useNotify();
+
   const allProductTypesFetcher = () => api.productTypes.getAll().then((result) => result.data);
   const {data: allProductTypes} = useSWR<ProductTypeType[]>('/product-type', allProductTypesFetcher);
 
-    return (
-      <div>
-        <CustomModal
-          onClose={() => props.onClose()}
-          closeOnClickBackground={true}
-          isActive={props.isActive}>
-          <div>
-            <h2>Create a New Variant</h2>
-            <h5>Add a new variant </h5>
+  const [uploadedImages, setUploadedImages] = useState([]);
 
-            <Formik
-              initialValues={{
-                variants: [{}],
-              }}
-              onSubmit={async (submitValues, {setSubmitting}) => {
+  return (
+    <div>
+      <CustomModal
+        onClose={() => props.onClose()}
+        closeOnClickBackground={true}
+        isActive={props.isActive}>
+        <div>
+          <h2>Create a New Variant</h2>
+          <h5>Add a new variant </h5>
 
-              }}
+          <Formik
+            initialValues={{
+              variants: [{}],
+            }}
+            onSubmit={async (submitValues, {setSubmitting}) => {
+
+              const variants = submitValues.variants.map((variant: any) => {
+                const {options, ...rest} = variant;
+                return {
+                  ...rest,
+                  optionValues: options.map(option => {
+                    return {
+                      uuid: option.attribute.uuid,
+                      value: option.attribute.value,
+                    };
+                  }),
+                  product: {
+                    uuid: props.productId,
+                  },
+                };
+              });
+
+              try {
+                await api.variants.create({
+                  payload: variants,
+                });
+                notify.success('Created variant successfully');
+                props.onClose();
+              } catch (e) {
+                console.error(e);
+                notify.error('Could not create variant due to an error');
+              }
+            }}
             >
-              {({values, isSubmitting}) => (
-                <Form>
-                  <div>
-                    <FieldArray
-                      name={'variants'}
-                      render={arrayHelpers => (
+            {({values, errors, isSubmitting}) => (
+              <Form>
+                <div>
+                  <FieldArray
+                    name={'variants'}
+                    render={arrayHelpers => (
+                      <div>
                         <div>
-                          <div>
-                            {
-                              values.variants?.map((variant, index) => (
-                                <div>
+                          {
+                            values.variants?.map((variant, index) => (
+                              <div>
                                   <CreateVariantComponent
                                     variantIndex={index}
                                     onDeleteVariant={(index) => arrayHelpers.remove(index)}
@@ -75,9 +105,8 @@ const CreateVariantModal = (props: {
                             </Column>
                             <Column>
                               <Button
+                                type={'submit'}
                                 isColor={'primary'}
-                                onClick={() => {
-                                }}
                                 style={{width: '100%'}}>
                                 Create Variant
                               </Button>
