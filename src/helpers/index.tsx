921a39d4f6df53ec3f2d951b0dff9232fff2c9ab
Brad <bradleykigsley2@gmail.com>
2021-09-21 15:07:13 +0300

    chore: adds null check

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -202,9 +202,12 @@ class DefaultHelpers {
   }
 
   titleCase(str: string) {
-    return str.split(' ')
-      .map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
-      .join(' ');
+    if (str) {
+      return str.split(' ')
+        .map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
+        .join(' ');
+    }
+    return '';
   }
 }
 
