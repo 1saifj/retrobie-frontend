58e6f99837076d0f1957ad56762df458dc86b295
Brad <bradleykigsley2@gmail.com>
2021-10-15 09:57:21 +0300

    refactor: convert to nested components

--- src/pages/landing/components/Categories.tsx
+++ src/pages/landing/components/Categories.tsx
@@ -20,123 +20,167 @@ const Categories = () => {
     return (
       <EmptyState
         icon={ServerError}
-        title={"Oops! That's an error. It's on us."}
-        message={"We couldn't fetch this section. Our best engineers have been notified, and are working on it."}
+        title={'Oops! That\'s an error. It\'s on us.'}
+        message={'We couldn\'t fetch this section. Our best engineers have been notified, and are working on it.'}
       />
     );
   }
 
   if (!allCategories) {
-        return <Loading minor={true}/>
-    }
+    return <Loading minor={true} />;
+  }
 
-    return (
-      <Section>
-        <Container>
-          <Parent>
-            <header style={{display: 'flex'}}>
-              <img
-                alt={'small blue shoe'}
-                src={CategoryTitle}
-                style={{width: '54px', padding: '8px'}}/>
-              <h2>Shop by category</h2>
-            </header>
-            <div className={'list'}>
-              {
-                allCategories.slice(0, 4).map(item => (
-                  <Link
-                    key={item.name}
-                    className="category--link"
-                    to={`/category/${item.slug}`}>
-                    <Category>
-                      <header>
-                        <h4>{item.name}</h4>
-                        <div className={'next'}>
-                          <img src={ChevronRight} alt={'go'}/>
-                        </div>
-                      </header>
-                      <div>
-                        <div style={{
-                          background:
-                          // a bit hacky, but it works for now
-                            item.slug.includes("women") ? "#45c0c7"
-                              : item.slug.includes("men") ? "#ba3f38"
-                              : item.slug.includes("kids") ? "#d13b78"
-                                : item.slug.includes("affordable") ? "#fdb813"
-                                  : item.slug.includes("popular") ? "#f5f5f5"
-                                    : "",
-                          textAlign: 'center',
-                          borderRadius: '2px',
-                          minWidth: '250px',
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          minHeight: '380px',
-                          flex: '1 1 250px'
-                        }}>
-                          <img src={`${item.landingImage?.url}?tr=w-250`} alt={item.name}/>
-                        </div>
-                      </div>
-                    </Category>
-                  </Link>
-                ))
-              }
-            </div>
-          </Parent>
-        </Container>
-      </Section>
-    );
+  return (
+    <Section>
+      <Container>
+        <>
+          <CategoriesHeader />
+          <CategoriesList categories={allCategories} />
+        </>
+      </Container>
+    </Section>
+  );
 };
 
-const Parent = styled.div`
-    
-  .list {
-    display: flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-    gap: 12px;
-  }
+const CategoriesHeader = () => {
+
+  return (
+    <>
+      <header style={{display: 'flex'}}>
+        <img
+          alt={'small blue shoe'}
+          src={CategoryTitle}
+          style={{width: '54px', padding: '8px'}} />
+        <h2>Shop by category</h2>
+      </header>
+    </>
+  );
+};
+
+const CategoriesList = ({categories}) => {
+
+  return (
+    <>
+      <CategoriesListContainer>
+        {
+          categories.slice(0, 4).map(item => <CategoryItem item={item} />)
+        }
+      </CategoriesListContainer>
+    </>
+  );
+};
+
+const CategoriesListContainer = styled.div`
+    display: grid;;
+    grid-template-columns: repeat(auto-fill, minmax(14em, 1fr));
+    gap: 1.8em;
 `;
 
+const CategoryItem = ({item}) => {
 
-const Category = styled('div')`
+  return (
+    <>
+      <Link
+        key={item.name}
+        className="category--link"
+        to={`/category/${item.slug}`}>
+        <CategoryItemContainer>
+          <CategoryItemHeader name={item.name} />
+          <CategoryItemImage
+            name={item.name}
+            slug={item.slug}
+            landingImage={item.landingImage}
+          />
+        </CategoryItemContainer>
+      </Link>
+
+    </>
+  );
+};
+
+const CategoryItemContainer = styled('div')`
   transition: all 0.25s ease-in-out;
   
   &:hover {
     cursor:pointer;
     transform: translateY(-4px);
   }  
+`;
+
+const CategoryItemHeader = ({name}) => {
+
+  return (
+    <>
+      <CategoryItemHeaderContainer>
+        <h4>{name}</h4>
+        <div className={'next'}>
+          <img src={ChevronRight} alt={'go'} />
+        </div>
+      </CategoryItemHeaderContainer>
+    </>
+  );
+};
+
+const CategoryItemHeaderContainer = styled.header`
+      display: flex;
+      justify-content:  space-between;
+      align-items: center;
+      padding: 12px 0;
+      
+      h4 {
+        margin: 0;
+      }
+      
+    .next {
+      background: #F5F6F7;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 28px;
+      height: 28px;
+      padding: 8px 8px 8px 6px;
+    }
+
+`;
+
+const CategoryItemImage = ({name, slug, landingImage}) => {
+  return (
+    <>
+      <CategoryItemImageContainer>
+        <div style={{
+          background:
+          // a bit hacky, but it works for now
+            slug.includes('women') ? '#45c0c7'
+              : slug.includes('men') ? '#ba3f38'
+              : slug.includes('kids') ? '#d13b78'
+                : slug.includes('affordable') ? '#fdb813'
+                  : slug.includes('popular') ? '#f5f5f5'
+                    : '',
+          textAlign: 'center',
+          borderRadius: '2px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: '380px',
+        }}>
+          <img src={`${landingImage?.url}?tr=w-250`} alt={name} />
+        </div>
+
+      </CategoryItemImageContainer>
+    </>
+  );
+};
+
+const CategoryItemImageContainer = styled.div`
   h4 {
     margin: 1em 1em 0;
   }
   
-  header {
-    display: flex;
-    justify-content:  space-between;
-    align-items: center;
-    padding: 12px 0;
-    
-    h4 {
-      margin: 0;
-  }
-    
-  }
-  
-  .next {
-    background: #F5F6F7;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 28px;
-    height: 28px;
-    padding: 8px 8px 8px 6px;
-  }
   img {
     border-radius: 2px;
     max-height: 375px;
   }
-
 `;
 
 export default Categories;
